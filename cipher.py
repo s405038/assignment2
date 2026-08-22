388e1f93116e4f0379df943790ccbb7ba6d65036
navan <s405038@students.cdu.edu.au>
2026-08-22 18:35:34 +1000

    initial commit

---
+++ cipher.py
@@ -0,0 +1,7 @@
+# cipher.py reads the text file raw_text.txt, 
+# encrypts its contents using the described scheme, and writes the result to 
+# encrypted_text.txt. It then has a function that decrypts that file, and a 
+# function that verifies the decryption was successful. 
+
+
+
