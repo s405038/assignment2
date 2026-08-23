aefee769f5dbb75f077b619071341019f0766464
navan <s405038@students.cdu.edu.au>
2026-08-23 11:11:41 +1000

    prog template ready for fine-tuning

--- cipher.py
+++ cipher.py
@@ -1,9 +1,10 @@
-# cipher.py reads the text file raw_text.txt, 
-# encrypts its contents using the described scheme, and writes the result to 
-# encrypted_text.txt. It then has a function that decrypts that file, and a 
-# function that verifies the decryption was successful. 
+# cipher.py reads the text file raw_text.txt,
+# encrypts its contents using the described scheme, and writes the result to
+# encrypted_text.txt. It then has a function that decrypts that file, and a
+# function that verifies the decryption was successful.
 
-def shiftlogic(input,shift):
+
+def shiftlogic(input, shift):
     """
     Gets the input and shift, calculates the shifted value
     and if it special characters, recalculates the value to point to alphabets
@@ -17,24 +18,27 @@ def shiftlogic(input,shift):
     """
 
     new_value = ord(input) - shift
-    if new_value < ord('A'):
+    if new_value < ord("A"):
         new_value += 26
-    
+
     return chr(new_value)
 
-def encrypt_file(shift1: int, shift2: int, input_path: str, output_path: str) :
-    input_text =  getfiletext(input_path) 
-    encrypted_text=encrypt(input_text,shift1,shift2)
+
+def encrypt_file(shift1: int, shift2: int, input_path: str, output_path: str):
+    input_text = getfiletext(input_path)
+    encrypted_text = encrypt(input_text, shift1, shift2)
 
     with open(output_path, "w") as file:
         file.write(encrypted_text)
 
-def decrypt_file(shift1: int, shift2: int, input_path: str, output_path: str) :
-    input_text = getfiletext(input_path)    
-    decrypted_text=decrypt(input_text,shift1,shift2)
+
+def decrypt_file(shift1: int, shift2: int, input_path: str, output_path: str):
+    input_text = getfiletext(input_path)
+    decrypted_text = decrypt(input_text, shift1, shift2)
 
     with open(output_path, "w") as file:
         file.write(decrypted_text)
+
 
 def getfiletext(path):
     inputfile = open(path, "r")
@@ -45,59 +49,63 @@ def getfiletext(path):
 
 
 def verify_files(original_path: str, decrypted_path: str):
-    input_text = getfiletext(original_path)    
-    output_text = getfiletext(decrypted_path) 
-    
-    if input_text==output_text:
+    input_text = getfiletext(original_path)
+    output_text = getfiletext(decrypted_path)
+
+    if input_text == output_text:
         return True
     else:
         return False
 
-def encrypt(input:str, shift1:int, shift2:int):
-   
-    lowercase_first_half ='abcdefghijklmn'
-    uppercase_first_half ='ABCDEFGHIJKLMN'
-    lowercase_second_half ='opqrstuvwxyz'
-    uppercase_second_half ='OPQRSTUVWXYZ'    
+
+def encrypt(input: str, shift1: int, shift2: int):
+
+    lowercase_first_half = "abcdefghijklmn"
+    uppercase_first_half = "ABCDEFGHIJKLMN"
+    lowercase_second_half = "opqrstuvwxyz"
+    uppercase_second_half = "OPQRSTUVWXYZ"
 
     # calculated shifts
-    multiplied_shift=shift1*shift2
-    added_shift=shift1+shift2
+    multiplied_shift = shift1 * shift2
+    added_shift = shift1 + shift2
 
-    encrypted_text="" # initialize enc text
+    encrypted_text = ""  # initialize enc text
 
     for char in input:
         if char in lowercase_first_half:
-            # If the letter is in the first half of the alphabet shift forward by shift1 * shift2 positions        
+            # If the letter is in the first half of the alphabet shift forward by shift1 * shift2 positions
             encrypted_text += chr(ord(char) + multiplied_shift)
         elif char in lowercase_second_half:
-            # If the letter is in the second half of the alphabet shift backward by shift1 + shift2 positions          
+            # If the letter is in the second half of the alphabet shift backward by shift1 + shift2 positions
             encrypted_text += chr(ord(char) - added_shift)
-        elif char in uppercase_first_half: 
-            # If the letter is in the first half shift backward by shift1 positions       
-            encrypted_text += shiftlogic(char,shift1)
+        elif char in uppercase_first_half:
+            # If the letter is in the first half shift backward by shift1 positions
+            encrypted_text += shiftlogic(char, shift1)
         elif char in uppercase_second_half:
-            # if the letter is in the second half shift forward by (shift2 squared)    
+            # if the letter is in the second half shift forward by (shift2 squared)
             encrypted_text += chr(ord(char) + shift2 * shift2)
         elif char.isdigit():
-            # Shift forward by shift1 - shift2         
+            # Shift forward by shift1 - shift2
             encrypted_text += chr(ord(char) + shift1 - shift2)
         else:
-            # Spaces, tabs, newlines, punctuation, symbols remain unchanged 
+            # Spaces, tabs, newlines, punctuation, symbols remain unchanged
             encrypted_text += char
 
     return encrypted_text
-        
-def decrypt(input):
 
+
+def decrypt(input,shift1, shift2):
+    
     return input
+
 
 def verify(firststring, secondstring):
 
-    if firststring==secondstring:
+    if firststring == secondstring:
         return True
     else:
         return False
+
 
 def validateinput(stringvalue):
     """
@@ -110,7 +118,7 @@ def validateinput(stringvalue):
     Returns:
         The validated input as an integer.
     """
-     
+
     user_input = input("Enter a number for " + stringvalue + ": ")
 
     while not user_input.isdigit():
@@ -120,17 +128,22 @@ def validateinput(stringvalue):
     return int(user_input)
 
 
-shift1=validateinput("shift1")
-shift2=validateinput("shift2")
+shift1 = validateinput("shift1")
+shift2 = validateinput("shift2")
 encrypt_file(shift1, shift2, "raw_text.txt", "encrypted_text.txt")
 print("Generated encrypted file")
 
-#input_text="a1A fxP"
-#encry_text=encrypt(input_text)
-#print(encry_text)
-#decrypted_text=decrypt(encry_text)
-#print(verify(encry_text,decrypted_text))
+decrypt_file(shift1, shift2, "encrypted_text.txt", "decrypted_text.txt")
+print("Generated decrypted file")
 
+if verify_files("raw_text.txt", "decrypted_text.txt")==True:
+    print("Decryption successful")
+else:
+    print("Decryption failed")
+    
 
-
-
+# input_text="a1A fxP"
+# encry_text=encrypt(input_text)
+# print(encry_text)
+# decrypted_text=decrypt(encry_text)
+# print(verify(encry_text,decrypted_text))
